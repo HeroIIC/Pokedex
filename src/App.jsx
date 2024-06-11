61723dd4ec74594a7c86b6fcf44e72444c65a1ec
HeroIIC <ventouse1@gmail.com>
2024-06-11 15:05:39 +0200

    added something for Two sec and delete it GREAT

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import MyTitle from "./MyTitle"
 import PokemonCard from './components/PokemonCard'
+const PokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <PokemonCard/>
+      <PokemonCard Pokemon={PokemonList[0]}/>
       
     </>
   )
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,6 @@
-function PokemonCard() {
-    const Pokemon = PokemonList[0];
+function PokemonCard({Pokemon}) {
+   
+    
     return (
         <><figure>
   {Pokemon.imgSrc ?(<img src={Pokemon.imgSrc} alt={Pokemon.name} />)
@@ -11,14 +12,6 @@ function PokemonCard() {
         </figure><img className="Bulbasaur" /></>
     )
 }
-const PokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+
+
 export default PokemonCard
