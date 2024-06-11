2a6b67f737545fe8fdf65747f55c07e1aff714d3
HeroIIC <ventouse1@gmail.com>
2024-06-11 14:26:13 +0200

    Quest03 done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,24 @@
 function PokemonCard() {
+    const Pokemon = PokemonList[0];
     return (
-        <figure>
-           
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
-        
-        </figure>
-        
+        <><figure>
+  {Pokemon.imgSrc ?(<img src={Pokemon.imgSrc} alt={Pokemon.name} />)
+  : (<p>???</p>) }
+    
+    
+            <figcaption>{Pokemon.name}</figcaption>
+     
+        </figure><img className="Bulbasaur" /></>
     )
 }
+const PokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 export default PokemonCard
